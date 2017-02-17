8885d27a52a502910d336368c0f64f974612de04
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-02-16 18:01:35 -0700

    Styles Browse page, begins home card styles

--- onmv/src/components/Browse.js
+++ onmv/src/components/Browse.js
@@ -3,14 +3,19 @@ import { Link } from 'react-router';
 
 const Browse = () => {
   return (
-    <section>
-      <Link id='browse-nav' to='/browse/types'>
-        <h3> Browse Events by Type </h3>
-       </Link>
+    <section >
 
-      <Link id='browse-nav' to="/browse/topics">
+      <article className='browse-nav'>
+        <Link  to='/browse/types'>
+          <h3> Browse Events by Type </h3>
+         </Link>
+      </article>
+
+      <article className='browse-nav'>
+        <Link to="/browse/topics">
         <h3> Browse Events by Topic </h3>
-      </Link>
+        </Link>
+      </article>
 
     </section>
   );
