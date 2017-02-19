2ba74252b5b9c0060e0836eac980ee7cbaa3380f
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-02-18 19:46:16 -0700

    Adds UI markdown for header, navigation, home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Home = () => {
   return (
     <section>
-      <article> Home </article>
+      <article className='home-card'>
+
+        <h3> Event Title </h3>
+        <button> + </button>
+        <h5>
+          <span> Event Date </span>
+          <span> Event Time </span>
+        </h5>
+        <p> Description of Event </p>
+        <label>
+          WR
+        </label>
+      </article>
+
     </section>
   );
 };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,11 +4,22 @@ import { Link } from 'react-router';
 const Navigation = () => {
   return (
     <nav>
-      <p> Navigation </p>
-      <Link to="/"> HOME </Link>
-      <Link to="/topics"> TOPICS </Link>
-      <Link to="/types"> TYPES </Link>
-      <Link to="/dashboard"> Dashboard </Link>
+      <Link id='home-nav' to='/'>
+        <img src="./icons/home-icon.svg"/>
+       </Link>
+
+      <Link id="browse-nav" to="/browse">
+        <img src="./icons/grid-icon.svg"/>
+      </Link>
+
+      <Link id="location-nav" to="/location">
+        <img src="./icons/location-icon.svg"/>
+      </Link>
+
+      <Link id="dashboard-nav" to="/dashboard">
+        <img src="./icons/person-icon.svg"/>
+      </Link>
+
     </nav>
   );
 };
