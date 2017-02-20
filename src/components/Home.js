a2fd826111956c97d9b27f0018c3ec8feba7e377
ccgamble <christine.c.gamble@gmail.com>
2017-02-20 12:31:07 -0700

    Populates event cards with JSON mock data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import EventCard from './EventCard';
 
-const Home = () => {
-  return (
-    <section>
-      <EventCard />
-    </section>
-  );
-};
+class Home extends Component {
+  render () {
+    let data = this.props.data
+    return (
+      <section>
+        {data.map(d => 
+          <EventCard id={d.id} name={d.name} date={d.date} time={d.time} desc={d.description} city={d.city} state={d.city} location={d.location} />
+          
+        )}
+      </section>
+    );
+  };
+}
 
 module.exports = Home;
