4acca7da1840dfe1928a07dc767e7a48048ce333
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-02-28 11:41:47 -0700

    Adds passing search tests

--- tests/location.test.js
+++ tests/location.test.js
@@ -3,9 +3,18 @@ import { Link } from 'react-router';
 import { shallow } from 'enzyme';
 import Location from '../src/components/Location';
 
+const sinon = require('sinon');
+
 describe('Location', () => {
   xit('renders a section element', () => {
     const wrapper = shallow(<Location />)
     expect(wrapper.type(), 'section')
+  });
+
+  xit('should call the set location function when the dropdown is changed', () =>{
+    let setLocation = sinon.spy();
+    const wrapper = mount(<Location setLocation={setLocation}/>);
+    wrapper.find('#dropdown').simulate('change');
+    expect(setLocation).to.have.property('callCount', 1);
   });
 });
