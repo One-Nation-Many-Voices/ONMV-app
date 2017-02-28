9e24a576fe689bba7552ba74fa14709aea7c8020
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-02-28 12:12:38 -0700

    Adds types component unit tests

--- tests/types.test.js
+++ tests/types.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router';
 import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import { assert, expect } from 'chai';
 import Types from '../src/components/Types';
 
 describe('Types', () => {
   it('renders a section element', () => {
-    const wrapper = mount(<Types />)
+    const wrapper = mount(<MemoryRouter><Types/></MemoryRouter>)
     expect(wrapper.type(), 'section')
   });
+
+  it('renders the TypesButton component', () => {
+    const wrapper = mount(<MemoryRouter><Types/></MemoryRouter>);
+    assert.equal(wrapper.find('.types-nav').length, 6);
+  });
+
 });
