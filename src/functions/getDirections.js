b70b6f7d3b6e46f793b29a46d1c0a7e8bd5d29d9
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-02-28 19:47:49 -0700

    Fixes get directions function

--- src/functions/getDirections.js
+++ src/functions/getDirections.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 module.exports = function getDirections(location, state, city) {
   if (location !== '' || location !== null) {
-    let directionsURL = `https://www.google.com/maps/place/${location}${city}${state}`
+    let directionsURL = `https://www.google.com/maps/place/+${location}+${city}+${state}`
     return <a className='address-icon' target='_blank' href={directionsURL}></a>
   }
   else {
