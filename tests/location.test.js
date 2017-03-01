a19ed6a31d851762faa15d8e5dc5e87a78960dbc
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-03-01 16:51:21 -0700

    Fixes header link and updates minor formatting

--- tests/location.test.js
+++ tests/location.test.js
@@ -3,19 +3,22 @@ import { Link } from 'react-router';
 import { shallow, mount } from 'enzyme';
 import Location from '../src/components/Location';
 import App from '../src/App';
+import Dropdown from 'react-dropdown';
+import { assert, expect } from 'chai';
 
 const sinon = require('sinon');
 
 describe('Location', () => {
-  xit('renders a section element', () => {    
-    const wrapper = shallow(<Location setLocation={'California'}/>)
+  it('renders a section element', () => {
+    let setLocation = sinon.spy();
+    const wrapper = shallow(<Location setLocation={setLocation} location={'California'}/>)
     expect(wrapper.type(), 'section')
   });
 
-  xit('should call the set location function when the dropdown is changed', () =>{
+  it('should call the set location function when the dropdown is changed', () =>{
     let setLocation = sinon.spy();
-    const wrapper = mount(<Location setLocation={setLocation}/>);
-    wrapper.find('#dropdown').simulate('change');
-    expect(setLocation).to.have.property('callCount', 1);
+    const wrapper = mount(<Location setLocation={setLocation} location={'California'}/>);
+    wrapper.find(Dropdown).simulate('change');
+    assert.equal(wrapper.props().setLocation.called, true);
   });
 });
