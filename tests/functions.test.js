156b7400e23d020c8e8ce9a5aafb9248f12e5d3e
ccgamble <christine.c.gamble@gmail.com>
2017-02-28 22:16:17 -0700

    Adds test for renderEventcard

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -12,7 +12,7 @@ describe('displayMessageNoEvents', function() {
 
   it('displays a message when there is no data', function() {
     let message = displayMessageNoEvents(noData);
-    expect(message).to.eql('No Events Matching the Type and/or Location Selected')
+    expect(message).to.eql('No Events Matching the Type and/or Location Selected');
   });
 
   it('does not display a message when there is data', function() {
@@ -29,29 +29,24 @@ describe('getDirections', function() {
   let link = <a className='address-icon' target='_blank' href={directionsURL}></a>
 
   it('should an link tag if a location is passed in', function() {
-    let directions = getDirections(location, state, city)
-    expect(directions).to.eql(link)
+    let directions = getDirections(location, state, city);
+    expect(directions).to.eql(link);
   })
 })
 
 describe('filterSearch', function() {
   let data = MockData[0]
-  
-  it('should create a string with all of the data', function() {
-    let result = filterSearch(data, 'town')
-    expect('result').to.be.a('string');
-  });
-    
+      
   it('should return the data if the search string is included', function() {
-    let result = filterSearch(data, 'Town Hall')
+    let result = filterSearch(data, 'Town Hall');
     expect(result).to.eql(true);
   });
     
   it('should return the data regardless of upper or lowercase search', function() {
-    let result = filterSearch(data, 'GARY')
+    let result = filterSearch(data, 'GARY');
     expect(result).to.eql(true);
     
-    result = filterSearch(data, 'gary')
+    result = filterSearch(data, 'gary');
     expect(result).to.eql(true);
   });
     
@@ -59,4 +54,30 @@ describe('filterSearch', function() {
     let result = filterSearch(data, 'REALLYLONGWORDTHATDOESNTEXIST');
     expect(result).to.eql(false);
   });
+});
+
+describe('renderEventCard', function() {
+  let data = MockData[0]
+  let eventCard = renderEventCard(data);
+  let properties = Object.keys(eventCard.props)
+  
+  it('should return an object', function() {
+    expect(eventCard).to.be.a('object');
+  })
+  
+  it('should include data properties', function() {
+    expect(properties).to.include('id');
+    expect(properties).to.include('id')
+    expect(properties).to.include('party')
+    expect(properties).to.include('eventType')
+    expect(properties).to.include('firstName')
+    expect(properties).to.include('lastName')
+    expect(properties).to.include('district')
+    expect(properties).to.include('date')
+    expect(properties).to.include('time')
+    expect(properties).to.include('street')
+    expect(properties).to.include('city')
+    expect(properties).to.include('state')
+    expect(properties).to.include('location')
+  })
 })
