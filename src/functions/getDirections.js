793a41d8d3cbb85bf17aeb69c817ec500740f33e
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-02-28 19:43:09 -0700

    Breaks out get directions function

---
+++ src/functions/getDirections.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+module.exports = function getDirections(location, state, city) {
+  if (location !== '' || location !== null) {
+    let directionsURL = `https://www.google.com/maps/place/${location}${city}${state}`
+    return <a className='address-icon' target='_blank' href={directionsURL}></a>
+  }
+  else {
+    return null
+  };
+};
